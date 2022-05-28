4b26ad8962d74f210672231a2387aa7e74e4e89d
Luciano S <luciano.sinisterra@lamercantil.com.ar>
2022-05-27 23:22:31 -0300

    Probando framer motion

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,52 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { motion } from 'framer-motion';
+import './App.css';
+import Board from './components/Board';
+import CardItem from './components/CardItem';
+import Grid from '@mui/material/Grid';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div>
+      <div className="board-tasks">
+        <Grid container spacing={2}>
+          <Grid item>
+            <Board></Board>
+          </Grid>
+          <Grid item>
+            <Board></Board>
+          </Grid>
+          <Grid item>
+            <Board></Board>
+          </Grid>
+        </Grid>
+      </div>
+
+      <motion.div
+        drag
+        whileDrag={{ scale: 1.2 }}
+        className="App">
+        <CardItem taskName="Clean kitchen"></CardItem>
+      </motion.div>
+
+
+      <motion.div
+        drag
+        whileDrag={{ scale: 1.2 }}
+        className="App">
+        <CardItem taskName="Clean kitchen"></CardItem>
+      </motion.div>
+
+
+      <motion.div
+        drag
+        whileDrag={{ scale: 1.2 }}
+        className="App">
+        <CardItem taskName="Cook cookies"></CardItem>
+      </motion.div>
+
+      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Board.jsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import './Board.css';
+
+
+export default function Board() {
+  return (
+    <Card sx={{ width: 380, height: 750, background: 'gray' }}>
+      <CardContent>
+        Tasks:
+
+
+       <section>
+
+       </section>
+      </CardContent>
+
+    </Card>
+  );
+}
